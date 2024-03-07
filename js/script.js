58597f05419d52929b8d96a985ac51e74659ee82
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-07 16:09:16 +0100

    Add: Bonus 2 - colored boxes - Finish project

--- js/script.js
+++ js/script.js
@@ -2,13 +2,7 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100,
 //ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-//Prima di partire a scrivere codice poniamoci qualche domanda:
-//Come faccio a sapere se un numero è divisibile per un altro?
-//Abbiamo visto qualcosa di particolare che possiamo usare?
 
-//Consigli del giorno:
-//1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-//2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
 //BONUS 1:
 //Crea un container nel DOM , aggiungendo un elemento html con il numero o 
@@ -18,10 +12,8 @@
 //Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*,
 // a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
-//Mi raccomando facciamo il bonus solo dopo aver completato la consegna base e averne testato il corretto funzionamento.
 
-
-
+//START
 
 //  selettore per il container creato nel DOM, 
 //in cui inserirò box con all'interno valori del ciclo FOR.
@@ -31,7 +23,7 @@ const numbContainer = document.querySelector('#numbers-container');
 for (let i = 1; i <= 100; i++) {
    
     let numberMod;
-    //Per i multipli di 3 stampi “Fizz” al posto del numero.  
+    //Per i multipli di 3 e di 5 stampi “Fizzbuz” al posto del numero.  
     if (i % 3 === 0 && i % 5 === 0) {
         numberMod = "FizzBuzz";
     } else if (i % 3 === 0) {
@@ -40,16 +32,32 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 5 === 0) {
         // Per i multipli di 5 stampi “Buzz” al posto del numero.
         numberMod = "Buzz";
+        // altrimenti stampi valore di i.
     } else {
         numberMod = i;
     }
 
+    //BONUS 1//
      // creo costante che crea un elemento <div>
      const newBox = document.createElement('div');
      // all'interno dell'elemento creato inserisco come contenuto i valori del loop for.
      newBox.innerHTML = numberMod;
      // all'elemento creato aggiungo la classe .box creata precedentemente nel css.
      newBox.classList.add('box');
+
+    //BONUS 2//
+    // creo condizione per aggiungere una classe alle box (bg-color nel css),
+    // in base ai risultati delle condizioni precedenti si applicano
+    // classi bg-color qui selezionate:
+    
+     if (numberMod === "Fizz") {
+        newBox.classList.add('fizz');
+    } else if (numberMod === "Buzz") {
+        newBox.classList.add('buzz');
+    } else if (numberMod === "FizzBuzz") {
+        newBox.classList.add('fizzbuzz');
+    }
+
      // "appendo" l'elemento appena creato  al container presente nel DOM.
      numbContainer.append(newBox);
 
